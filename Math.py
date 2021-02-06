eec5713e6381398462f95c7cf2fd997b5b2614f3
Laith Tareq <laith96.t@gmail.com>
2021-02-06 20:36:18 +0200

    6/2/2021

--- Math.py
+++ Math.py
@@ -1,4 +1,5 @@
 import math
 #print(math.sin(math.radians(30)))
-round(0.78647,2)
-print(round(math.sin(math.radians(30)),2))
+#round(0.78647,2)
+#print(round(math.sin(math.radians(30)),2))
+
---
+++ String.py
@@ -0,0 +1,15 @@
+#Name = "   Python Tariq  "
+#print(dir(Name))
+#print(Name.endswith(".gpj"))
+#print(Name.istitle())
+#print(Name.split("T"))
+#print(Name)
+#print(Name.strip())
+#L = Name.split()
+#print(L)
+#print(''.join(L))
+#print(Name.strip())
+x = 5
+y = 10
+Data = "x = {}, y = {}".format(x,y)
+print(Data)
